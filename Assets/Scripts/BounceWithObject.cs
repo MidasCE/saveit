153ca38d583fd2dbdc298736e6bd6e955cb279a3
pattarapol_tiensermpong <midaskmitl@gmail.com>
2025-04-17 14:17:24 +0700

    Bouncing object script, attached to fish.

---
+++ Assets/Scripts/BounceWithObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BounceWithObject : MonoBehaviour
+{
+    [SerializeField] private string bounceTag;
+    [SerializeField] private float bounceSpeed = 4f;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(bounceTag))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+
+            // Optional: only bounce if falling downwards
+            
+            // Bounce upwards
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, bounceSpeed, rb.linearVelocity.z);
+        }
+    }
+}
