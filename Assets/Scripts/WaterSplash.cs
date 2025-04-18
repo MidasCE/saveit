56d65bcf53fb58684c1a0f75ec4f9a64849f3a7e
pattarapol_tiensermpong <midaskmitl@gmail.com>
2025-04-18 14:27:45 +0700

    Water splash when dropped fish into the bowl

---
+++ Assets/Scripts/WaterSplash.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaterSplash : MonoBehaviour
+{
+    [SerializeField] private Renderer waterRenderer;
+    [SerializeField] private string shaderProperty = "_Displacement";
+    [SerializeField] private float splashValue = 1.3f;
+    [SerializeField] private float startValue = 0.46f;
+    [SerializeField] private float duration = 1.5f;
+
+    private Coroutine _splashCoroutine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{other.name} entered the water!");
+        if (waterRenderer == null) 
+            return;
+        if (_splashCoroutine != null)
+            StopCoroutine(_splashCoroutine);
+
+        waterRenderer.material.SetFloat(shaderProperty, splashValue);
+        _splashCoroutine = StartCoroutine(SplashWaterThroughShader());
+    }
+
+    private IEnumerator SplashWaterThroughShader()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float newVal = Mathf.Lerp(splashValue, startValue, elapsed / duration);
+            waterRenderer.material.SetFloat(shaderProperty, newVal);
+            yield return null;
+        }
+    }
+}
